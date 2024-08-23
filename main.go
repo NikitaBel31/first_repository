f37f3eb72c913f738dbce1ab7ffaec67ca76a4fe
Nikita Shtokolov <shtokolov2003@mail.ru>
2024-08-23 19:08:15 +0300

    Буферезированный канал

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
-	a := make(chan int)
+	a := make(chan int, 2)
 	go someFunc(a)
+	fmt.Println(<-a)
 	fmt.Println(<-a)
 	fmt.Println("Вот что вывелось")
 
 }
 func someFunc(a chan int) {
 	a <- 12
+	a <- 10
 }
